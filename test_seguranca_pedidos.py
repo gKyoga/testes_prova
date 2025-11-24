b2eea1f9007733211900ef2d04ef6b0f80e12d26
Gustavo Vieira <147004139+gKyoga@users.noreply.github.com>
2025-11-24 20:01:37 -0300

    Update order_id in security test case

--- test_seguranca_pedidos.py
+++ test_seguranca_pedidos.py
@@ -2,8 +2,9 @@ from sistema_pedidos import buscar_detalhes_pedido
 
 def test_access_to_foreign_order_must_be_denied():
     user_id = 2
-    order_id = 101  
+    order_id = 2
 
     output = buscar_detalhes_pedido(order_id, user_id)
 
     assert output == {}, "Violação de segurança: acesso não autorizado ao pedido deve ser devolvido."
+
